0afca25699f6d26041724be7fe47830ad72a2d70
hedu <eperezghedu@gmail.com>
2021-04-24 18:43:52 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/elasticsearchtest/ElasticsearchTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.elasticsearchtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ElasticsearchTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<ElasticsearchTestApplication>(*args)
+}
