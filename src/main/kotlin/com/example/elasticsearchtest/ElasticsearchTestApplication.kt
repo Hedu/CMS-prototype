22ea8eb035e09fc7bd22a2beaacb231887965d82
hedu <eperezghedu@gmail.com>
2021-05-01 11:56:48 +0200

    Enforce index creation on startup

--- src/main/kotlin/com/example/elasticsearchtest/ElasticsearchTestApplication.kt
+++ src/main/kotlin/com/example/elasticsearchtest/ElasticsearchTestApplication.kt
@@ -1,7 +1,10 @@
 package com.example.elasticsearchtest
 
+import com.example.elasticsearchtest.model.documents.Document
+import com.example.elasticsearchtest.model.documents.DocumentType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations
 
 
 @SpringBootApplication
@@ -9,6 +12,10 @@ class ElasticsearchTestApplication
 
 fun main(args: Array<String>) {
 	val applicationContext = runApplication<ElasticsearchTestApplication>(*args)
+
+	val elasticsearchOperations = applicationContext.getBean(ElasticsearchOperations::class.java)
+	elasticsearchOperations.indexOps(DocumentType::class.java).create()
+	elasticsearchOperations.indexOps(Document::class.java).create()
 }
 
 
